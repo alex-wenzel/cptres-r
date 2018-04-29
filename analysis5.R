8d0cb3455d8c044907b64ef556f37726149499cc
alex-wenzel <atwenzel@eng.ucsd.edu>
2018-04-29 23:42:19 +0000

    full 28k with original 10x clustering

---
+++ analysis5.R
@@ -0,0 +1,54 @@
+### This analysis uses the original clustering from 10X K=9 at
+### /mnt/oncogxA/Projects/CPTRES/RNAExpression/10x/aggregates/10x_5_15/outs/analysis/clustering/kmeans_9_clusters
+
+library(Seurat)
+library(dplyr)
+library(stringr)
+
+## This function implements cell-cycle scoring
+## should be run after scaling/variable genes
+
+scdata <- Read10X(data.dir="full_28k/raw28k/")
+sc <- CreateSeuratObject(raw.data=scdata, min.cells=3, min.genes=3, project="10X_CPTRES")
+
+get.pop.name <- function(bc) {
+  strsplit(bc, "-")[[1]][2]
+}
+
+barcode.src <- data.frame(sc@raw.data@Dimnames[[2]])
+names(barcode.src) <- c("src")
+barcode.src$src <- as.numeric(lapply(as.character(barcode.src$src), get.pop.name))
+rownames(barcode.src) <- sc@raw.data@Dimnames[[2]]
+
+sc <- AddMetaData(object=sc, metadata=barcode.src, col.name="barcode.src")
+
+VlnPlot(object=sc, features.plot=c("nGene", "nUMI"), nCol=2, point.size.use=0.5)
+
+GenePlot(object=sc, gene1="nUMI", gene2="nGene")
+
+sc <- NormalizeData(object=sc, normalization.method="LogNormalize", scale.factor=10000)
+
+sc <- FindVariableGenes(object=sc, mean.function=ExpMean, dispersion.function=LogVMR,
+                        x.low.cutoff=0.0125, x.high.cutoff=3, y.cutoff=0.5)
+
+## This needs a lot of memory
+sc <- ScaleData(object=sc)
+
+## Swaps out sc@ident to the external clustering
+new.ident <- as.factor(read.csv("full_28k/orig_clustering/clusters.csv")$Cluster)
+attributes(new.ident)$names <- attributes(sc@ident)$names
+sc@ident <- new.ident
+
+## PCA
+sc <- RunPCA(object=sc, pc.genes=sc@var.genes)
+PCAPlot(object=sc, dim.1=1, dim.2=2)
+
+## tSNE
+sc <- RunTSNE(object=sc, dims.use=1:10, do.fast=TRUE)
+TSNEPlot(object=sc)
+TSNEPlot(object=sc, group.by='src')
+
+## DEG
+sc.markers <- FindAllMarkers(object=sc, min.pct=0.25, thresh.use=0.25)
+top10 <- sc.markers %>% group_by(cluster) %>% top_n(10,avg_logFC)
+DoHeatmap(object=sc, genes.use=top10$gene, slim.col.label=TRUE, remove.key=TRUE)
