293d440f9f6021f1dcfe9874fc5525c778a60251
alex-wenzel <atwenzel@eng.ucsd.edu>
2018-05-12 03:33:18 +0000

    plot color updates

--- analysis12.R
+++ analysis12.R
@@ -8,7 +8,6 @@ library(GSA)
 library(pheatmap)
 library(parallel)
 library(matrixStats)
-library(RColorBrewer)
 
 ## Mapping of barcode suffix number to experiment name
 exp.names <- c("r06_5", "r18_5", "s02", "s03", "r14_5", "r06_15",
@@ -26,7 +25,7 @@ sc <- CreateSeuratObject(raw.data=scdata, min.cells=3, min.genes=3, project="10X
 
 ## Normalize and scale 10X data w/Seurat
 sc <- NormalizeData(object=sc, normalization.method="LogNormalize", scale.factor=10000)
-#sc <- ScaleData(object=sc)
+sc <- ScaleData(object=sc)
 
 ## Load cell cycle data
 cc.genes <- readLines(con="satija_cell_cycle/regev_lab_cell_cycle_genes.txt")
@@ -45,9 +44,12 @@ names(barcode.src) <- c("src")
 barcode.src$src <- lapply(as.character(barcode.src$src), get.pop.name)
 rownames(barcode.src) <- sc@raw.data@Dimnames[[2]]
 
+## Convert sc@data to a basic matrix
+expr.mat <- as.matrix(sc@scale.data)
+
 ## Convert to matrix preserving rows with < n% zeros
 n.zero.lim = 10
-prevalent.genes <- as.matrix(sc@scale.data[rowSums(sc@data==0)<=n.zero.lim,])
+prevalent.genes <- as.matrix(sc@scale.data[rowSums(sc@scale.data==0)<=n.zero.lim,])
 
 ## Find Seurat Clusters
 sc <- RunPCA(object=sc, pc.genes=rownames(prevalent.genes), do.print=F)
@@ -85,29 +87,5 @@ anno.col <- data.frame(
   Seurat.Cluster=sc@ident
 )
 rownames(anno.col) <- colnames(prevalent.genes)
-all.colors <- c(
-  "#c8d24d",
-  "#8a49c7",
-  "#6dcb60",
-  "#c64d91",
-  "#87cab7",
-  "#d1503b",
-  "#7388c5",
-  "#c69a56",
-  "#442f59",
-  "#4b643e",
-  "#cc9fb0",
-  "#723a33")
-hm.anno.colors <- list(
-  Experiment=all.colors,
-  Orig.Cluster=all.colors[1:7],
-  Cell.Cycle=all.colors[1:3],
-  Seurat.Cluster=all.colors[1:4]
-)
-names(hm.anno.colors$Experiment) <- attr(anno.col$Experiment, "levels")
-names(hm.anno.colors$Orig.Cluster) <- attr(anno.col$Orig.Cluster, "levels")
-names(hm.anno.colors$Cell.Cycle) <- attr(anno.col$Cell.Cycle, "levels")
-names(hm.anno.colors$Seurat.Cluster) <- attr(anno.col$Seurat.Cluster, "levels")
 pheatmap(prevalent.genes, show_rownames=F, show_colnames=F, 
-         cluster_rows=F, annotation_col=anno.col,
-         annotation_colors=hm.anno.colors)
+         cluster_rows=F, annotation_col=anno.col)
